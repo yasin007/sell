1293215532a75d2fe02dfdc0b15e744474232d74
毅杨 <yangyi@gogopay.cn>
2018-06-25 11:18:04 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="header">
+      i am header
+    </div>
+    <div class="tab">
+      i am tab
+    </div>
+  </div>
+</template>
+<script>
+  import header from './components/header/header.vue';
+
+  export default {
+    components: {
+      header
+    }
+  };
+</script>
+
+<style>
+
+</style>
