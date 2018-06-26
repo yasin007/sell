07dae70eee3641523834b934c7fbb48ed943613c
shine0007 <shine@weiapp.com>
2018-06-27 00:27:28 +0800

    [Feature]提交代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
-//
-// Vue.use(Router)
-//
-// export default new Router({
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'HelloWorld',
-//       component: HelloWorld
-//     }
-//   ]
-// })
+import Vue from 'vue';
+import Router from 'vue-router';
+import Header from '@/components/header/header';
+import goods from '@/components/goods/goods';
+import ratings from '@/components/ratings/ratings';
+import seller from '@/components/seller/seller';
+Vue.use(Router);
+export default new Router({
+  linkActiveClass: 'list-active',
+  routes: [
+    {
+      path: '/header',
+      name: 'header',
+      component: Header
+    },
+    {
+      path: '/goods',
+      name: goods,
+      component: goods
+    },
+    {
+      path: '/ratings',
+      name: ratings,
+      component: ratings
+    },
+    {
+      path: '/seller',
+      name: seller,
+      component: seller
+    }
+  ]
+});
