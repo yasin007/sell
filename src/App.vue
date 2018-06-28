39a6a123b008e418dcc0229f1643cfe9ed4459ab
毅杨 <yangyi@gogopay.cn>
2018-06-28 18:39:27 +0800

    [Feature]提交代码

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,12 @@
   import MHeader from 'components/m-header/m-header'
   import Tab from 'components/tab/tab'
 
-  export default {components: {MHeader, Tab}}
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
